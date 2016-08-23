f9ffc1466147c68939886bbc21d1dceb9e81fa7c
Jim Thoburn <jim@jimthoburn.com>
2016-08-22 20:22:12 -0700

    Add files via upload

--- public/local.js
+++ public/local.js
@@ -9,36 +9,36 @@ sendButton.addEventListener("click", sendMessage);
 
 // if ENTER key was pressed, send message
 window.addEventListener("keypress", function(event){
-	if (event.which === 13) {		
+	if (event.which === 13) {
 		sendMessage();
-	}	
+	}
 });
 
 // when "chat" event received, display message
 socket.on('chat', function(data){
-    console.log('RECEIVED: name: '+ data.name + ', message: ' + data.message);
+	console.log('RECEIVED: name: '+ data.name + ', message: ' + data.message);
 	displayNewMessage(data.name, data.message);
 });
 
 
 function sendMessage(event) {
 	console.log('SENDING: name: '+ nameInput.value + ', message: ' + messageInput.value);
-	socket.emit('chat', {name: nameInput.value, message: messageInput.value} );  
+	socket.emit('chat', {name: nameInput.value, message: messageInput.value} );
+	messageInput.value = '';
 }
 
 function displayNewMessage (username, message) {
-  var newMessage = document.createElement('div');
-  newMessage.className = 'message';
-  var messageTextNode = document.createTextNode(': ' + message);
+	var newMessage = document.createElement('div');
+	newMessage.className = 'message';
+	var messageTextNode = document.createTextNode(': ' + message);
 
-  var newMessageUser = document.createElement('span');
-  newMessageUser.className = 'username';
-  newMessageUser.innerText = username;
-  newMessage.appendChild(newMessageUser);
-  newMessage.appendChild(messageTextNode);
+	var newMessageUser = document.createElement('span');
+	newMessageUser.className = 'username';
+	newMessageUser.innerText = username;
+	newMessage.appendChild(newMessageUser);
+	newMessage.appendChild(messageTextNode);
 
-  document.getElementById('chat').appendChild(newMessage);
+	document.getElementById('chat').appendChild(newMessage);
 
-  window.scrollTo(0,document.body.scrollHeight);
+	window.scrollTo(0,document.body.scrollHeight);
 }
-
